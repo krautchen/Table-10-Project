df62389672acee8c549e59330b2d7f2cb02b8c74
Richard Salters <ras0404@gmail.com>
2024-04-07 19:05:38 -0400

    error message and condensing

--- driver.py
+++ driver.py
@@ -8,8 +8,6 @@
 -add the items to each of the rooms after functionality for such has been 
 created
 """
-#---------notes for team---------
-"""this is a huge fucking mess just give me some time"""
 
 class Map:
     def __init__(self, choice):
@@ -28,6 +26,9 @@ class Rooms(Map):
         self.status(self.currentRoom)
         super().__init__(choice)
         self.defaultVal = True
+        self.invalidDirection = "invalid choice, please choose another directon"
+        self.prompt = "Which direction would you like to go? "
+        self.neswError = "please choose a movement of nesw" #are we going to tell them what map theyre on?
         
 
     def selection(self):
@@ -35,83 +36,149 @@ class Rooms(Map):
            self.threeByThree("5")
            self.status(self.currentRoom)
     
-    #optimize later?
+    #optimize later? my eyes hurt staring at this shit
     def threeByThree(self, choice):
         #default starting value is 5
+        
         if self.defaultVal == True:
             choice = 5
             self.defaultVal = False
         if choice == 5:
             self.currentRoom = 5
             print(self.status(self.currentRoom))
-            userInput = input("Which direction would you like to go? ") 
+            userInput = input(self.prompt) 
             p5 = {"n": 2, "e": 6, "s": 8, "w": 4}
+
+            if userInput not in p5:
+                  self.neswError()
+
             choice = p5.get(userInput)
+            if choice == "invalid movement":
+                self.invalidMovementMethod()
 
         if choice == 1:
-            p1 = {"n": 1, "e": 2, "s": 4, "w": None} #p1 = {"n": 1, "e": 2, "s": 4, "w": 1}
+            p1 = {"n": 1, "e": 2, "s": 4, "w": "invalid movement"} 
             self.currentRoom = 1
             print(self.status(self.currentRoom))
-            userInput = input("Which direction would you like to go? ") 
+            userInput = input(self.prompt) 
+
+            if userInput not in p1:
+                self.neswError()
+                self.threeByThree(self.currentRoom)
+
             choice = p1.get(userInput)
+            if choice == "invalid movement":
+                self.invalidMovementMethod()
             self.threeByThree(choice)
 
         elif choice == 2:
-            p2 = {"n": None, "e": 3, "s": 5, "w": 1}
+            p2 = {"n": "invalid movement", "e": 3, "s": 5, "w": 1}
             self.currentRoom = 2
             print(self.status(self.currentRoom))
-            userInput = input("Which direction would you like to go? ") 
+            userInput = input(self.prompt) 
+
+            if userInput not in p2:
+                self.neswError()
             choice = p2.get(userInput)
+
+            if choice == "invalid movement":
+                self.invalidMovementMethod()
             self.threeByThree(choice)
 
         elif choice == 3:
-            p3 = {"n": None, "e": None, "s": 6, "w": 2}
+            p3 = {"n": "invalid movement", "e": "invalid movement", 
+                  "s": 6, "w": 2}
             self.currentRoom = 3
             print(self.status(self.currentRoom))
-            userInput = input("Which direction would you like to go? ") 
+            userInput = input(self.prompt) 
+
+            if userInput not in p3:
+                self.neswError()
+
             choice = p3.get(userInput)
+            if choice == "invalid movement":
+                self.invalidMovementMethod()
             self.threeByThree(choice)
 
         elif choice == 4:
-            p4 = {"n": 1, "e": 5, "s": 6, "w": None}
+            p4 = {"n": 1, "e": 5, "s": 6, "w": "invalid movement"}
             self.currentRoom = 4
             print(self.status(self.currentRoom))
-            userInput = input("Which direction would you like to go? ") 
+            userInput = input(self.prompt) 
+
+            if userInput not in p4:
+                self.neswError()
+
             choice = p4.get(userInput)
+            if choice == "invalid movement":
+                self.invalidMovementMethod()
             self.threeByThree(choice)
 
         elif choice == 6:
-            p6 = {"n": 3, "e": None, "s": 9, "w": 5}
+            p6 = {"n": 3, "e": "invalid movement", "s": 9, "w": 5}
             self.currentRoom = 6
             print(self.status(self.currentRoom))
-            userInput = input("Which direction would you like to go? ") 
+            userInput = input(self.prompt) 
+
+            if userInput not in p6:
+                self.neswError()
+
             choice = p6.get(userInput)
+            if choice == "invalid movement":
+                self.invalidMovementMethod()
             self.threeByThree(choice)
 
         elif choice == 7:
-            p7 = {"n": 4, "e": 8, "s": None, "w": None}
+            p7 = {"n": 4, "e": 8, "s": "invalid movement", 
+                  "w": "invalid movement"}
             self.currentRoom = 7
             print(self.status(self.currentRoom))
-            userInput = input("Which direction would you like to go? ") 
+            userInput = input(self.prompt) 
+
+            if userInput not in p7:
+                self.neswError()
+
             choice = p7.get(userInput)
+            if choice == "invalid movement":
+                self.invalidMovementMethod()
             self.threeByThree(choice)
 
         elif choice == 8:
-            p2 = {"n": 5, "e": 9, "s": None, "w": 7}
+            p8 = {"n": 5, "e": 9, "s": "invalid movement", "w": 7}
             self.currentRoom = 8
             print(self.status(self.currentRoom))
-            userInput = input("Which direction would you like to go? ") 
-            choice = p2.get(userInput)
+            userInput = input(self.prompt) 
+
+            if userInput not in p8:
+                self.neswError()
+
+            choice = p8.get(userInput)
+            if choice == "invalid movement":
+                self.invalidMovementMethod()
             self.threeByThree(choice)
 
         elif choice == 9:
-            p2 = {"n": 6, "e": None, "s": None, "w": 8}
+            p9 = {"n": 6, "e": "invalid movement", 
+                  "s": "invalid movement", "w": 8}
             self.currentRoom = 9
             print(self.status(self.currentRoom))
-            userInput = input("Which direction would you like to go? ") 
-            choice = p2.get(userInput)
+            userInput = input(self.prompt) 
+
+            if userInput not in p9:
+                self.neswError()
+
+            choice = p9.get(userInput)
+            if choice == "invalid movement":
+                self.invalidMovementMethod()
             self.threeByThree(choice)
-           
+
+    def invalidMovementMethod(self):
+        print(self.invalidDirection)
+        self.threeByThree(self.currentRoom)
+
+    def neswErrorMethod(self):
+        print(self.neswError)
+        self.threeByThree(self.currentRoom)
        
             
     def oneByFive(self):
