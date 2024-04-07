8fae4c0b07e3b910f3a768ab6f14ef0bf9e146e8
Richard Salters <ras0404@gmail.com>
2024-04-07 12:12:47 -0400

    dictionary map

--- driver.py
+++ driver.py
@@ -5,7 +5,8 @@
 -sorting out move limitations 
 -when move specified correct room is applied
 -add non square/rectangle rooms
-
+-add the items to each of the rooms after functionality for such has been 
+created
 """
 #---------notes for team---------
 """this is a huge fucking mess just give me some time"""
@@ -26,22 +27,40 @@ class Rooms(Map):
         self.currentRoom = 0
         self.status(self.currentRoom)
         super().__init__(choice)
+        self.defaultVal = True
         
 
     def selection(self):
         if self.super.choice == "3x3":
-           self.threeByThree()
+           self.threeByThree("5")
            self.status(self.currentRoom)
-           
-    def threeByThree(self):
-        if "1":
-            self.neswSetter([False, True, True, False])
+    
+    #optimize later?
+    def threeByThree(self, choice):
+        #default starting value is 5
+        if self.defaultVal == True:
+            userInput = input("Which direction would you like to go? ") 
+            p5 = {"n": 2, "e": 6, "s": 8, "w": 4}
+            choice = p5.get(userInput)
+            self.defaultVal = False
+
+        if choice == 1:
+            p1 = {"n": None, "e": 2, "s": 4, "w": None}
             self.currentRoom = 1
             print(self.status(self.currentRoom))
-            
-        elif "2":
-            self.neswSetter([False, True, True, True])
+            userInput = input("Which direction would you like to go? ") 
+            choice = p1.get(userInput)
+            self.threeByThree(choice)
+
+        elif choice == 2:
+            p2 = {"n": None, "e": 3, "s": 5, "w": 1}
             self.currentRoom = 2
+            print(self.status(self.currentRoom))
+            userInput = input("Which direction would you like to go? ") 
+            choice = p2.get(userInput)
+            self.threeByThree(choice)
+           
+       
             
     def oneByFive(self):
         None
@@ -54,4 +73,4 @@ class Rooms(Map):
 
    
 c = Rooms("1")
-c.threeByThree()
+c.threeByThree("1")
