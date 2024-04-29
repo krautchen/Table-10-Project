a3745d5d252f981d52b5f2c680e17cdb4646e8da
Richard Salters <ras0404@gmail.com>
2024-04-28 20:24:40 -0400

    an abomination before God

--- driver.py
+++ driver.py
@@ -1,5 +1,6 @@
 import json
 import random
+#from puzzle import puzzle as p
 """Functionality check-list for map
 -building rooms
 -linking/pointing to appropriate rooms
@@ -8,7 +9,10 @@ import random
 -add non square/rectangle rooms
 -add the items to each of the rooms after functionality for such has been 
 created
+-add all the .upper shit to standardize the answers 
+-add main function
 """
+
 class Map:
     def __init__(self, choice):
         self.choice = choice
@@ -16,10 +20,11 @@ class Map:
         self.invalidDirection = "invalid choice, please choose another directon"
         self.neswError = "please choose a movement of nesw" 
         self.neswCheck = ["n", "s", "e", "w"]
+        self.playInput = "would you like to continue? "
+        self.gameEnd = False 
         #item and puzzle random and flag to say they already did it
 
     def mapDriver(self):
-        playInput = "would you like to continue? "
 
         with open ("rooms.json", "r", encoding= "utf-8") as f:
             map = json.load(f)
@@ -28,77 +33,111 @@ class Map:
         #picks a random map
         randomMap = random.choice(list(map))
         playRoom = map[randomMap]
-
         #picks random room in map
         randomRoom= str(random.randint(1, len(map[randomMap])))
 
         #game start
         currentRoom = playRoom[randomRoom]
+
+        #below for testing
+        #playRoom = map["1x5"]
+        #currentRoom = playRoom["4"]
         current = currentRoom["current"]
         print(self.status(current))
         userInput = input(self.prompt)
-
-        if userInput not in currentRoom:
-            self.neswErrorMethod()
-
-        if userInput not in self.neswCheck:
-            self.neswErrorMethod()
-            answer = input(playInput)
-            if answer == "no":
-                    play = False    
-        movement = currentRoom[userInput]
+        userInput.lower()
         
-        while(play == True):
-            previous = currentRoom["current"]
-            if userInput not in self.neswCheck:
-                self.neswErrorMethod()
-                answer = input(playInput)
-                if answer == "no":
-                    play = False
-                    break
-                else:
-                    currentRoom = playRoom[previous]
-                    current = currentRoom["current"]
+        if userInput not in currentRoom:
+            userInput = self.playPrompt("nesw")
 
-            if movement == "invalid movement":
-                self.invalidMovementMethod()
-                answer = input(playInput)
-                
-                if answer == "no":
-                    play = False
-                    break
-                else:
-                    currentRoom = playRoom[previous]
-                    current = currentRoom["current"]
+        if userInput not in self.neswCheck and self.gameEnd == False:
+            userInput = self.playPrompt("nesw")
+        elif self.gameEnd != True and userInput in currentRoom:
+            movement = currentRoom[userInput]
+      
+
+        while(self.gameEnd != True):
+            
+            previous = currentRoom["current"]
+
+            if userInput not in self.neswCheck:
+                 userInput = self.playPrompt("nesw")
+               
+            elif self.gameEnd != True:
+                currentRoom = playRoom[previous]
+                current = currentRoom["current"]
+
+            if movement == "invalid movement" and self.gameEnd == False:
+                userInput = self.playPrompt("lol")
+                while(userInput not in currentRoom and self.gameEnd != True):
+                    userInput = self.playPrompt("lol")
+                if self.gameEnd != True:
+                    movement = currentRoom[userInput]
+
+            if self.gameEnd == True:
+                        break
+            else:
+                currentRoom = playRoom[previous]
+                current = currentRoom["current"]
 
             if movement != "invalid movement":
                 currentRoom = playRoom[movement]
                 current = currentRoom["current"]
+           
 
             print(self.status(current))
             userInput = input(self.prompt)
-
+            userInput.lower()
+        
             if userInput not in currentRoom:
-                self.neswErrorMethod()
-                answer = input(playInput)
-                if answer == "no":
-                    play = False
-                    break
-            movement = currentRoom[userInput]
+                 userInput = self.playPrompt("nesw")
+            if self.gameEnd != True:
+                movement = currentRoom[userInput]
+           
         
     def __repr__(self) -> str:
         return self.choice
     
     def status(self, cr):
         return f"you are currently in room {cr}"
-    
-    def invalidMovementMethod(self):
-        print(self.invalidDirection)
         
-    def neswErrorMethod(self):
-        print(self.neswError)
+    def playPrompt(self, errorChoice):
+        ansFlag = True
+        once = False
+        if errorChoice == "nesw":
+            if once == False:
+                print(self.neswError)
+                once = True
+            answer = input(self.playInput)
+        else:
+            if once == False:
+                print(self.invalidDirection)
+                once = True
+            answer = input(self.playInput)
+        answer.lower()
+        while(ansFlag == True):
+            if answer == "no":
+                self.play = False 
+                ansFlag = False
+                print("goodbye!")
+                self.gameEnd = True
+                break
+            elif answer == "yes":
+                na = input(self.prompt)
+                if na in self.neswCheck:
+                    ansFlag = False
+                    return na
+                else:
+                    print("invalid answer, please try again.")
+                    answer = input(self.playInput)
+                    answer.lower()
+            else: 
+                    print("invalid answer, please try again.")
+                    answer = input(self.playInput)
+                    answer.lower()
+                    
+
 
 
 c = Map("3x3")
 c.mapDriver()
-
