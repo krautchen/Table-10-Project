fcb4c5a1dd0679213961970e722fb176eb877d54
krautchen <ndhibbl@terpmail.umd.edu>
2024-04-28 21:42:19 -0400

    Items working. Created main to start centralizing

---
+++ items.py
@@ -0,0 +1,78 @@
+import json
+def load_items(items_file):
+    items = {"weapons": [], "armor": [], "consumables": [], "bags": []}
+    with open(items_file, 'r') as file:
+        data = json.load(file)
+        for category, item_list in data.items():
+            for item_data in item_list:
+                if category == "weapons":
+                    item = Weapon(item_data['name'],
+                                  item_data['type'],
+                                  item_data['damage'])
+                elif category == "armor":
+                    item = Armor(item_data['name'],
+                                 item_data['armor_value'])
+                elif category == "consumables":
+                    item = Consumable(item_data['name'],
+                                      item_data['healing_value'])
+                elif category == "bags":
+                    item = Bag(item_data['name'],
+                               item_data['size'])
+                items[category].append(item)
+    return items
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+        
+class Weapon(Item):
+    def __init__(self, name, type, damage):
+        super().__init__(name)
+        self.type = type
+        self.damage = damage
+        
+    def __repr__(self):
+        return f"Weapon(name={self.name}, type={self.type}, damage={self.damage})"
+
+class Armor(Item):
+    def __init__(self, name, armor_value):
+        super().__init__(name)
+        self.armor_value = armor_value
+    def __repr__(self):
+        return f"Armor(name={self.name}, armor_value={self.armor_value})"
+
+class Consumable(Item):
+    def __init__(self, name, healing_value):
+        super().__init__(name)
+        self.healing_value = healing_value
+        
+    def __repr__(self):
+        return f"Consumable(name={self.name}, healing_value={self.healing_value})"
+
+        
+    def heal(self,target):
+        target.heal(self.healing_value)
+
+class Bag(Item):
+    def __init__(self, name, size, contents=[]):
+        super().__init__(name)
+        self.size = size
+        self.contents = contents
+        
+    def __repr__(self):
+        return f"Bag(name={self.name}, size={self.size})"
+        
+    def store(self,item):
+        self.contents.append(item)
+    
+    def remove(self,item):
+        self.contents.remove(item)
+
+    def organize(self):
+        sorted(self.contents,key=lambda bag: bag[1])
+        
+items=load_items('C:/Users/noahh/OneDrive/Documents/GitHub/Table-10-Project/fantasy_items.json')
+weapons = items['weapons']
+armor = items['armor']
+consumables = items['consumables']
+bags = items['bags']
---
+++ main.py
@@ -0,0 +1,35 @@
+
+class Game:
+    def __init__(self):
+    
+    def load_genre(self,genre):
+        fantasy = r"""^(f[a-z]*n[a-z]*t[a-z]*s[a-z]*y)$"""
+        scifi = r"""(?x)^(s[a-z]*c[a-z]*i[a-z]*
+                        e?n?c?e?f?
+                        [a-z]*i[a-z]*c?t?[a-z]*i[a-z]*o?n?)$"""
+
+    fantasy_regex = re.compile(fantasy, re.IGNORECASE)
+    scifi_regex = re.compile(fantasy, re.IGNORECASE)
+
+    if fantasy_regex.match(genre) == True:
+        load_items('fantasy_items.json')
+        load_monsters('fantasy_monsters.json')
+
+    elif scifi_regex.match(genre):
+        raise NotImplementedError("This genre isn't ready yet!")
+
+def main(genre):
+    game = Game(genre)
+    game.play()
+
+
+def parse_args(arglist):
+
+    parser = ArgumentParser()
+    parser.add_argument("genre", help="path to word list text file")
+    return parser.parse_args(arglist)
+
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+    main(args.genre)
