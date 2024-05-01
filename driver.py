0b2bcd9bee291f724abc48f50f6e0a547e71fe27
Richard Salters <ras0404@gmail.com>
2024-05-01 16:20:44 -0400

    docstring

--- driver.py
+++ driver.py
@@ -11,8 +11,34 @@ created
 """
 
 class Map:
+    """
+    The map that is being used.
+
+    Attributes:
+        prompt: string
+            a prompt asking which direction to travel
+        invalidDirection: string
+            a prompt explaining the direction choice is not valid
+        neswError: string
+            a prompt explaining to choose a valid cardinal direction
+        neswCheck: list
+            the list of valid cardinal directiosn
+        playInput: string
+            a prompt asking the user if they want to continue
+        gameEnd: boolean
+            signals if the game is over
+        puzzleEnd: string
+            signals if the puzzle has been run and completed
+        current: string
+            the current room that the player is in
+    
+    """
     def __init__(self):
-        #self.choice = choice
+        """
+        Initalizes a map object
+        side effects:
+            Initalizes all the values
+        """
         self.prompt = "Which direction would you like to go? "
         self.invalidDirection = "invalid choice, please choose another directon"
         self.neswError = "please choose a movement of nesw" 
@@ -21,16 +47,19 @@ class Map:
         self.gameEnd = False 
         self.puzzleEnd = "not complete"
         self.current = ""
-        
-        #load items and create objects for each of the item types and rand 
-        #between zero and two to see what those types are made
-        #look at the example file n shit for the one example
 
-    def mapDriver(self): #add back the filepath arguement
+    def mapDriver(self): 
+        """
+        The main driver for the users movement in the map
+
+        Side effects:
+            assigns the map and room at random read from a json file, 
+            and then handles movement for the duration of the game. 
+        """
         with open ("rooms.json", "r", encoding= "utf-8") as f:
             map = json.load(f)
         
-       #picks a random map
+        #picks a random map
         randomMap = random.choice(list(map))
         playRoom = map[randomMap]
         #picks random room in map
@@ -40,9 +69,6 @@ class Map:
         #game start
         currentRoom = playRoom[randomRoom]
 
-        #below for testing
-        #playRoom = map["1x5"]
-        #currentRoom = playRoom["4"]
         self.current = currentRoom["current"]
         print(self.status(self.current))
         userInput = input(self.prompt).lower()
@@ -100,14 +126,31 @@ class Map:
                  userInput = self.playPrompt("nesw")
             if self.gameEnd != True:
                 movement = currentRoom[userInput]
-        
-    #def __repr__(self) -> str:
-        #return self.choice
     
     def status(self, cr):
+        """
+        Displays what room the user is currently in
+
+        Args:
+        cr: string
+            The current room the user is
+
+        Returns:
+            returns the in a string explaining current positioning of the user    
+        """
         return f"you are currently in room {cr}"
         
     def playPrompt(self, errorChoice):
+        """
+        Handles errors for movement and invalid inputs.
+        Args:
+        errorChoice: string
+            the type of error to be displayed to the user
+        
+        Returns:
+            Either if the game is over or a valid movement for the user to 
+            traverse
+        """
         ansFlag = True
         once = False
         if errorChoice == "nesw":
@@ -140,5 +183,5 @@ class Map:
                     print("invalid answer, please try again.")
                     answer = input(self.playInput).lower()
 
-c = Map()
-c.mapDriver()
+#c = Map()
+#c.mapDriver()
